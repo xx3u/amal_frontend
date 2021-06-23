010e80603ea24d1d525b41e10aa84c5bd5ec6507
Zaur Kubanov <ravencount@gmail.com>
2021-06-23 21:12:29 +0600

    Создан компонент тестового Хэдера. Отображается при запуске.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,2 +1,33 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, Button} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+
+const Header =()=> {
+    const classes = useStyles();
+    return(
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+           ADMIN 
+          </Typography>
+          <Button color="inherit">Pressable</Button>
+        </Toolbar>
+        </AppBar>
+    )
+};
+
+export default Header;
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+
+const Layout =(props)=> {
+
+
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Layout;
