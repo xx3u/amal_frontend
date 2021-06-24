9c157af971ce895bbef20c0f4913d2365befd1f6
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-06-24 11:00:53 +0600

    Реализовал  redux-store saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/configureStore';
 
-ReactDOM.render(
-  <React.StrictMode>
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
