2b9b0a9e2b48321ff55624afd8e233c073dadd3c
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-06-28 15:31:37 +0600

    удалил redux-saga, установил redux-thunk, проект переделан на redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from "connected-react-router";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './store/configureStore';
+import store, {history} from "./store/configureStore";
 
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,25 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import createSagaMiddleware from '@redux-saga/core';
-import rootReducer from './reducers/rootReducer';
-import { rootSaga } from './sagas/index';
+import {createBrowserHistory} from "history";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import {connectRouter, routerMiddleware} from "connected-react-router";
+import thunkMiddleware from "redux-thunk";
+import mainReducer from './reducers/mainReducer'
+
+export const history = createBrowserHistory()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const sagaMiddleware = createSagaMiddleware();
+const rootReducers = combineReducers({
+  main: mainReducer,
+  router: connectRouter(history)
+});
 
-const middleware = [sagaMiddleware];
+const middleware = [
+  thunkMiddleware,
+  routerMiddleware(history)
+]
 
-const enhancers = composeEnhancers(applyMiddleware(...middleware));
+const enhancers = composeEnhancers(applyMiddleware(...middleware))
 
-const store = createStore(rootReducer, enhancers);
-sagaMiddleware.run(rootSaga);
+const store = createStore(rootReducers, enhancers);
 
-export default store;
+export default store;   
