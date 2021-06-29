e24057edc7a5bbb5fafb708615b73295e55aeee6
Yuliya Khvan <yuliya.khv@gmail.com>
2021-06-29 23:44:28 +0600

    Убран Grid

--- src/components/UI/Form/FormItem/FormItem.js
+++ src/components/UI/Form/FormItem/FormItem.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Grid, TextField } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 const FormItem = (props) => {
   return (
-    <Grid item xs={12}>
-      <TextField
-        variant='outlined'
-        fullWidth
-        required={props.required}
-        id={props.name}
-        type={props.type}
-        multiline={props.multiline}
-        rows={3}
-        label={props.label}
-        name={props.name} 
-        autoComplete={props.name}
-        value={props.value}
-        onChange={props.onChange}
-        className={props.className}
-        error={!!props.error}
-        helperText={props.error}
-      />
-    </Grid>
+    <TextField
+      variant='outlined'
+      fullWidth
+      required={props.required}
+      id={props.name}
+      type={props.type}
+      multiline={props.multiline}
+      rows={3}
+      label={props.label}
+      name={props.name} 
+      autoComplete={props.name}
+      value={props.value}
+      onChange={props.onChange}
+      className={props.className}
+      error={!!props.error}
+      helperText={props.error}
+    />
   )
 };
 
