fc917951a5041611ec01d82430633b6aa12705d5
Yuliya Khvan <yuliya.khv@gmail.com>
2021-06-29 21:12:02 +0600

    Удален reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './store/configureStore';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -15,8 +14,3 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
