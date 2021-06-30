636c4422c70df0354183a471131e77b4cf63aae3
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-06-30 18:45:49 +0600

    добавлены экшн о начале запроса,стейт loading в редюсере

--- src/store/actions/studentsAction.js
+++ src/store/actions/studentsAction.js
@@ -1,17 +1,16 @@
-import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_SUCCESS } from "../actionTypes"
-import axios from '../../axiosApi'
+import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_REQUEST, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
+import axios from '../../axiosApi';
 
-export const fetchStudentsSuccess = students => (
-     {type: FETCH_STUDENTS_SUCCESS, students}
-)
+export const fetchStudentsSuccess = students => ({type: FETCH_STUDENTS_SUCCESS, students});
 
-export const fetchStudentsFailure = error => (
-     {type: FETCH_STUDENTS_FAILURE, error}
-);
+export const fetchStudentsFailure = error => ({type: FETCH_STUDENTS_FAILURE, error});
+
+export const fetchStudentsRequest = () => ({type: FETCH_STUDENTS_REQUEST});
 
 export const fetchStudents = () => (
      async dispatch => {
         try {
+            dispatch(fetchStudentsRequest())
             const response = await axios.get('/')
             dispatch(fetchStudentsSuccess(response.data))
         } catch(error) {
--- src/store/reducers/studentsReducer.js
+++ src/store/reducers/studentsReducer.js
@@ -1,16 +1,19 @@
-import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
+import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_REQUEST, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
 
 const initialState = {
     students: [],
-    error: null
+    error: null,
+    loading: false
 };
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case FETCH_STUDENTS_REQUEST:
+        return {...state, loading: true}
     case FETCH_STUDENTS_SUCCESS:
-        return {...state, students: action.students}
+        return {...state, students: action.students, loading: false}
     case FETCH_STUDENTS_FAILURE: 
-        return {...state, error: action.error}
+        return {...state, error: action.error, loading: false}
     default:
       return state;
   }
