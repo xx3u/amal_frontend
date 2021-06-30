a5998db608ce23329ffdccda4a079dad4546c378
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-06-30 18:32:23 +0600

    добавил в редюсер стейт ошибку при получение данных студентов

--- src/store/reducers/studentsReducer.js
+++ src/store/reducers/studentsReducer.js
@@ -1,13 +1,16 @@
-import { FETCH_STUDENTS_SUCCESS } from "../actionTypes";
+import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
 
 const initialState = {
-    students: []
+    students: [],
+    error: null
 };
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case FETCH_STUDENTS_SUCCESS:
         return {...state, students: action.students}
+    case FETCH_STUDENTS_FAILURE: 
+        return {...state, error: action.error}
     default:
       return state;
   }
