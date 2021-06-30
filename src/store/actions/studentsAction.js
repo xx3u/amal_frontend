962d342069b9329ace6ccba4d7e3abff3487ef3c
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-06-30 18:11:49 +0600

    добавил экшн на ошибку получения данные студентов

--- src/store/actions/studentsAction.js
+++ src/store/actions/studentsAction.js
@@ -1,17 +1,25 @@
-import { FETCH_STUDENTS_SUCCESS } from "../actionTypes"
+import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_SUCCESS } from "../actionTypes"
 import axios from '../../axiosApi'
 
 export const fetchStudentsSuccess = students => (
      {type: FETCH_STUDENTS_SUCCESS, students}
 )
 
+export const fetchStudentsFailure = error => (
+     {type: FETCH_STUDENTS_FAILURE, error}
+);
+
 export const fetchStudents = () => (
      async dispatch => {
         try {
             const response = await axios.get('/')
             dispatch(fetchStudentsSuccess(response.data))
-        } catch(e) {
-            console.log(e)
+        } catch(error) {
+            if (error.response && error.response.data) {
+                dispatch(fetchStudentsFailure(error.response.data));
+            } else {
+                dispatch(fetchStudentsFailure(error));
+            }
         }
     }
 )
