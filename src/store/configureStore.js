d7458ae0b3109c90c78d97b84f089fda68684ab5
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-01 19:29:35 +0600

    Добавлен CreateBrowserHistory

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,23 @@
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 import studentsReducer from './reducers/studentsReducer'
 
+export const history = createBrowserHistory();
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-  students: studentsReducer
+  students: studentsReducer,
+  router: connectRouter(history)
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+];
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;   
