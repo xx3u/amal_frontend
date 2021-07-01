23145ed8f619c31bf67d7f92686eebc88c3636de
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-01 19:31:53 +0600

    Обновлен reducer на добавление нового студента

--- src/store/reducers/studentsReducer.js
+++ src/store/reducers/studentsReducer.js
@@ -1,18 +1,39 @@
-import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_REQUEST, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
+import {
+    ADD_NEW_STUDENT_FAILURE,
+  ADD_NEW_STUDENT_REQUEST,
+  ADD_NEW_STUDENT_SUCCESS,
+  FETCH_STUDENTS_FAILURE,
+  FETCH_STUDENTS_REQUEST,
+  FETCH_STUDENTS_SUCCESS,
+} from "../actionTypes";
 
 const initialState = {
-    students: [],
-    error: null,
-    loading: false
+  students: [],
+  error: null,
+  loading: false,
 };
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case FETCH_STUDENTS_REQUEST:
-        return {...state, loading: true}
+      return { ...state, loading: true };
     case FETCH_STUDENTS_SUCCESS:
-        return {...state, students: action.students, error: null, loading: false}
-    case FETCH_STUDENTS_FAILURE: 
+      return {
+        ...state,
+        students: action.students,
+        error: null,
+        loading: false,
+      };
+    case FETCH_STUDENTS_FAILURE:
+      return { ...state, error: action.error, loading: false };
+    case ADD_NEW_STUDENT_REQUEST:
+      return { ...state, loading: true };
+    case ADD_NEW_STUDENT_SUCCESS: {
+        const updatedStudents = [...state.students];
+        updatedStudents.push(action.data);
+        return {...state, students: updatedStudents, loading: false}
+    }
+    case ADD_NEW_STUDENT_FAILURE:
         return {...state, error: action.error, loading: false}
     default:
       return state;
