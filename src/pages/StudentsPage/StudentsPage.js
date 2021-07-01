9459d8a88b0d5ea49a69ee477118f4685bed782b
Zaur Kubanov <ravencount@gmail.com>
2021-07-01 15:04:15 +0600

    Элемент кнопки добавления учеников поставлен

--- src/pages/StudentsPage/StudentsPage.js
+++ src/pages/StudentsPage/StudentsPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from '@material-ui/core';
 import StudentsContainer from '../../containers/StudentsContainer/StudentsContainer';
 
+ 
 const StudentsPage = () => (
   <div>
       <h1>Ученики</h1>
+      <Button component={Link}
+                    to="/admin-app/students/add"
+                    color="primary">
+                      Добавить нового ученика
+      </Button>
       <StudentsContainer/>
   </div>
 );
