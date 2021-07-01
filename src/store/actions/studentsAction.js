efd56ca15fe3fd00c75104de33d777fab3a3bddf
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-01 19:31:16 +0600

    Добавлены actions на добавление нового студента

--- src/store/actions/studentsAction.js
+++ src/store/actions/studentsAction.js
@@ -1,24 +1,52 @@
-import { FETCH_STUDENTS_FAILURE, FETCH_STUDENTS_REQUEST, FETCH_STUDENTS_SUCCESS } from "../actionTypes";
-import axios from '../../axiosApi';
+import { push } from "connected-react-router";
+import {
+    ADD_NEW_STUDENT_FAILURE,
+  ADD_NEW_STUDENT_REQUEST,
+  ADD_NEW_STUDENT_SUCCESS,
+  FETCH_STUDENTS_FAILURE,
+  FETCH_STUDENTS_REQUEST,
+  FETCH_STUDENTS_SUCCESS,
+} from "../actionTypes";
+import axios from "../../axiosApi";
 
-export const fetchStudentsSuccess = students => ({type: FETCH_STUDENTS_SUCCESS, students});
+export const fetchStudentsSuccess = (students) => ({
+  type: FETCH_STUDENTS_SUCCESS,
+  students,
+});
 
-export const fetchStudentsFailure = error => ({type: FETCH_STUDENTS_FAILURE, error});
+export const fetchStudentsFailure = (error) => ({
+  type: FETCH_STUDENTS_FAILURE,
+  error,
+});
 
-export const fetchStudentsRequest = () => ({type: FETCH_STUDENTS_REQUEST});
+export const fetchStudentsRequest = () => ({ type: FETCH_STUDENTS_REQUEST });
 
-export const fetchStudents = () => (
-     async dispatch => {
-        try {
-            dispatch(fetchStudentsRequest())
-            const response = await axios.get('/students')
-            dispatch(fetchStudentsSuccess(response.data))
-        } catch(error) {
-            if (error.response && error.response.data) {
-                dispatch(fetchStudentsFailure(error.response.data));
-            } else {
-                dispatch(fetchStudentsFailure(error));
-            }
-        }
+export const fetchStudents = () => async (dispatch) => {
+  try {
+    dispatch(fetchStudentsRequest());
+    const response = await axios.get("/students");
+    dispatch(fetchStudentsSuccess(response.data));
+  } catch (error) {
+    if (error.response && error.response.data) {
+      dispatch(fetchStudentsFailure(error.response.data));
+    } else {
+      dispatch(fetchStudentsFailure(error));
     }
-)
+  }
+};
+
+const addNewStudentRequest = () => ({ type: ADD_NEW_STUDENT_REQUEST });
+const addNewStudentSuccess = (data) => ({ type: ADD_NEW_STUDENT_SUCCESS, data });
+const addNewStudentFailure = (error) => ({type: ADD_NEW_STUDENT_FAILURE, error});
+
+export const addNewStudent = (newStudent) => async (dispatch) => {
+    dispatch(addNewStudentRequest());
+    try {
+        await axios
+          .post("/students", newStudent)
+          .then((response) => dispatch(addNewStudentSuccess(response.data)));
+          dispatch(push('/admin-app/students'));
+    } catch (error) {
+        dispatch(addNewStudentFailure(error));
+    }
+};
