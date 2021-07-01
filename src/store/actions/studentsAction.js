bb48f5c672247af97d41e2c9f48766bbd063d489
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-01 14:54:44 +0600

    исправил endpoint отправки запроса на получение данных студентов

--- src/store/actions/studentsAction.js
+++ src/store/actions/studentsAction.js
@@ -11,7 +11,7 @@ export const fetchStudents = () => (
      async dispatch => {
         try {
             dispatch(fetchStudentsRequest())
-            const response = await axios.get('/')
+            const response = await axios.get('/students')
             dispatch(fetchStudentsSuccess(response.data))
         } catch(error) {
             if (error.response && error.response.data) {
