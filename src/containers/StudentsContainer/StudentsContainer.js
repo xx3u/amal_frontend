5ee87f303d6d6c58ea572d250f295460c90357fd
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-05 12:47:06 +0600

    удалены необязательные поля в контейнере Students

--- src/containers/StudentsContainer/StudentsContainer.js
+++ src/containers/StudentsContainer/StudentsContainer.js
@@ -7,14 +7,11 @@ const StudentsContainer = () => {
   const columns = [
     { field: 'id', headerName: 'ID', width: 100, headerAlign: 'center' },
     { field: 'lastName', headerName: 'Фамилия', width: 200, headerAlign: 'center' },
-    { field: 'firstName', headerName: 'Имя', width: 200, headerAlign: 'center' },
+    { field: 'firstName', headerName: 'Имя', width: 150, headerAlign: 'center' },
     { field: 'grade', headerName: 'Класс', type: 'number', width: 120, align: 'center', headerAlign: 'center' },
-    { field: 'school', headerName: 'Школа', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'language', headerName: 'Язык обучения', width: 230, align: 'center', headerAlign: 'center' },
-    { field: 'parentsContacts', headerName: 'Контакты родителей', width: 230, align: 'center', headerAlign: 'center' },
+    { field: 'language', headerName: 'Язык обучения', width: 200, align: 'center', headerAlign: 'center' },
     { field: 'stream', headerName: 'Направление', width: 170, align: 'center', headerAlign: 'center' },
-    { field: 'address', headerName: 'Адрес', width: 150, align: 'center', headerAlign: 'center' },
-    { field: 'telephone', headerName: 'Телефон ученика', width: 190, align: 'center', headerAlign: 'center' },
+    { field: 'school', headerName: 'Школа', width: 200, align: 'center', headerAlign: 'center' },
   ];
 
   const rows = useSelector((state) => state.students.students);
