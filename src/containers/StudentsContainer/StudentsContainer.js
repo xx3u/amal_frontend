b24d56b04c7bd4b145ca114628f6056a88ddaf72
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-05 13:47:12 +0600

    вравние колонок

--- src/containers/StudentsContainer/StudentsContainer.js
+++ src/containers/StudentsContainer/StudentsContainer.js
@@ -5,18 +5,19 @@ import { fetchStudents } from '../../store/actions/studentsAction';
 
 const StudentsContainer = () => {
   const columns = [
-    { field: 'id', headerName: 'ID', width: 100, headerAlign: 'center' },
-    { field: 'lastName', headerName: 'Фамилия', width: 200, headerAlign: 'center' },
-    { field: 'firstName', headerName: 'Имя', width: 150, headerAlign: 'center' },
-    { field: 'grade', headerName: 'Класс', type: 'number', width: 120, align: 'center', headerAlign: 'center' },
-    { field: 'language', headerName: 'Язык обучения', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'stream', headerName: 'Направление', width: 170, align: 'center', headerAlign: 'center' },
-    { field: 'school', headerName: 'Школа', width: 200, align: 'center', headerAlign: 'center' },
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'firstName', headerName: 'Имя', width: 150 },
+    { field: 'lastName', headerName: 'Фамилия', width: 200 },
+    { field: 'language', headerName: 'Язык обучения', width: 180 },
+    { field: 'stream', headerName: 'Направление', width: 170 },
+    { field: 'school', headerName: 'Школа', width: 150 },
+    { field: 'grade', headerName: 'Класс', type: 'number', width: 120 },
   ];
 
   const rows = useSelector((state) => state.students.students);
 
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchStudents());
   }, [dispatch]);
