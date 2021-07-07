02b814f988b30c50181244523f2b9f64c2e2126f
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-07 23:53:10 +0600

    Создана папка Student и перемещены соответствующие файлы

---
+++ src/containers/Forms/Student/CreateStudentForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import StudentForm from './StudentForm';
+import { useDispatch } from 'react-redux';
+import { addNewStudent } from '../../../store/actions/studentsAction';
+
+const CreateStudentForm = () => {
+  const student = useState({
+    firstName: '',
+    lastName: '',
+    middleName: '',
+    grade: '',
+    language: '',
+    school: '',
+    parentsContacts: '',
+    stream: '',
+    address: '',
+    telephone: '',
+    email: '',
+  });
+
+  const dispatch = useDispatch();
+
+  const addStudent = (student) => {
+    dispatch(addNewStudent(student));
+  };
+
+  return <StudentForm title={'Добавить ученика'} submitData={addStudent} selectedStudent={student[0]} />;
+};
+
+export default CreateStudentForm;
