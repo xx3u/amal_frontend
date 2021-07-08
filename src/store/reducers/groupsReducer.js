8a052bfa5294326292bfa4b1e73229d38d8c6940
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-08 19:04:38 +0600

    добавил редюсер для хранения стейтов добавления новых групп

--- src/store/reducers/groupsReducer.js
+++ src/store/reducers/groupsReducer.js
@@ -1,4 +1,11 @@
-import { FETCH_GROUPS_FAILURE, FETCH_GROUPS_REQUEST, FETCH_GROUPS_SUCCESS } from '../actionTypes';
+import {
+  ADD_NEW_GROUP_FAILURE,
+  ADD_NEW_GROUP_REQUEST,
+  ADD_NEW_GROUP_SUCCESS,
+  FETCH_GROUPS_FAILURE,
+  FETCH_GROUPS_REQUEST,
+  FETCH_GROUPS_SUCCESS,
+} from '../actionTypes';
 
 const initialState = {
   groups: [],
@@ -18,6 +25,15 @@ const reducer = (state = initialState, action) => {
         loading: false,
       };
     case FETCH_GROUPS_FAILURE:
+      return { ...state, error: action.error, loading: false };
+    case ADD_NEW_GROUP_REQUEST:
+      return { ...state, loading: true };
+    case ADD_NEW_GROUP_SUCCESS: {
+      const updatedGroups = [...state.groups];
+      updatedGroups.push(action.data);
+      return { ...state, groups: updatedGroups, loading: false };
+    }
+    case ADD_NEW_GROUP_FAILURE:
       return { ...state, error: action.error, loading: false };
     default:
       return state;
