b95833c571867ccac67b4d493da7669b53277525
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-08 13:49:19 +0600

    на главном контейнере групп вызвал функцию получения данных групп с экшн через юзефект

--- src/containers/GroupsContainer/GroupsContainer.js
+++ src/containers/GroupsContainer/GroupsContainer.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import TableItems from '../../components/TableItems/TableItems';
+import { fetchGroups } from '../../store/actions/groupsAction';
 
 const GroupsContainer = () => {
   const columns = [
     { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'name', headerName: 'Наименование', width: 200 },
+    { field: 'groupName', headerName: 'Наименование', width: 200 },
   ];
 
   const rows = useSelector((state) => state.groups.groups);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGroups());
+  }, [dispatch]);
 
   return <TableItems rows={rows} columns={columns} />;
 };
