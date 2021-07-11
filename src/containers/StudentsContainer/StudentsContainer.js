f3175175e87a0cad1b979b90ca7d6cb56be7018a
Zaur Kubanov <ravencount@gmail.com>
2021-07-12 02:12:21 +0600

    Поставлена кнопка для будущего перехода на страницу студента. Нужно отредактировать выравнивание текста кнопок

--- src/containers/StudentsContainer/StudentsContainer.js
+++ src/containers/StudentsContainer/StudentsContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import StudentsTable from '../../components/TableItems/TableItems';
 import { fetchStudents } from '../../store/actions/studentsAction';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const StudentsContainer = () => {
@@ -18,12 +18,12 @@ const StudentsContainer = () => {
     {
       field: 'Действия',
       sortable: false,
-      width: 200,
+      width: 260,
       sortable: false,
       disableClickEventBubbling: true,
       renderCell: function cellEdit(params) {
         return (
-          <div>
+          <Grid container direction='row' justify='space-evenly'>
             <Button
               variant='contained'
               color='default'
@@ -42,7 +42,7 @@ const StudentsContainer = () => {
             >
               Детали         
             </Button>
-          </div>
+          </Grid>
         );
       },
     },
