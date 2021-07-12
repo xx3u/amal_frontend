2f67cd78d12b53af40474383e6c9f97bb14819a1
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-12 15:57:12 +0600

    Добавлена заглушка для таблицы списка студентов

--- src/containers/GroupsContainer/GroupsContainer.js
+++ src/containers/GroupsContainer/GroupsContainer.js
@@ -3,12 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import MyAccordion from '../../components/UI/MyAccordion/MyAccordion';
 import SimpleTable from '../../components/UI/SimpleTable/SimpleTable';
 import { fetchGroups } from '../../store/actions/groupsAction';
-import { fetchStudents } from '../../store/actions/studentsAction';
 
 const GroupsContainer = () => {
   const dispatch = useDispatch();
   const rows = useSelector((state) => state.groups.groups);
-  const students = useSelector((state) => state.students.students);
 
   const columns = [
     { field: 'firstName', headerName: 'Имя', width: 150 },
@@ -16,9 +14,13 @@ const GroupsContainer = () => {
     { field: 'grade', headerName: 'Класс', type: 'number', width: 120 },
   ];
 
+  const students = [
+    { id: 1, firstName: 'Серік', lastName: 'Ахметов', grade: 5 },
+    { id: 2, firstName: 'Арман', lastName: 'Тасболат', grade: 2 },
+  ];
+
   useEffect(() => {
     dispatch(fetchGroups());
-    dispatch(fetchStudents());
   }, [dispatch]);
 
   return (
