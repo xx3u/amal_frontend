35c6a4623723b87514f4da520790d83123af290b
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-12 23:30:18 +0600

    Исправлены замечания капитана(Юля): настроен форматирование prettier, укорочен код импорта

--- src/components/UI/MyAccordion/MyAccordion.js
+++ src/components/UI/MyAccordion/MyAccordion.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles, Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,11 +21,7 @@ const MyAccordion = ({ groupName, children }) => {
   return (
     <div className={classes.root}>
       <Accordion className={classes.accordion}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
           <Typography className={classes.heading}>{groupName}</Typography>
         </AccordionSummary>
         <AccordionDetails>{children}</AccordionDetails>
