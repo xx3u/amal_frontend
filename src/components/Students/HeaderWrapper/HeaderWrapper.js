593815083ea3059862e1298e1bdde7800b607a4e
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-14 21:45:46 +0600

    Создана обертка для шапки в странице Students. В эту обертку передан Button в качестве дочерного элемента

---
+++ src/components/Students/HeaderWrapper/HeaderWrapper.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeaderWrapper = ({ children }) => {
+  return <div className='HeaderWrapper'>{children}</div>;
+};
+
+export default HeaderWrapper;
--- src/pages/StudentsPage/StudentsPage.js
+++ src/pages/StudentsPage/StudentsPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import StudentsContainer from '../../containers/StudentsContainer/StudentsContainer';
+import HeaderWrapper from '../../components/Students/HeaderWrapper/HeaderWrapper';
 
 const useStyles = makeStyles((theme) => ({
   addButton: {
@@ -15,15 +16,17 @@ const StudentsPage = () => {
   return (
     <div>
       <h1>Ученики</h1>
-      <Button
-        variant='contained'
-        component={Link}
-        to='/admin-app/students/add'
-        color='default'
-        className={classes.addButton}
-      >
-        Добавить нового ученика
-      </Button>
+      <HeaderWrapper>
+        <Button
+          variant='contained'
+          component={Link}
+          to='/admin-app/students/add'
+          color='default'
+          className={classes.addButton}
+        >
+          Добавить нового ученика
+        </Button>
+      </HeaderWrapper>
       <StudentsContainer />
     </div>
   );
