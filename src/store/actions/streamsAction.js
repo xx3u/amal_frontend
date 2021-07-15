b3429c2113c6796031e1e1bc046d0a2274ba3148
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-07-15 13:22:12 +0600

    Добавил actions

---
+++ src/store/actions/streamsAction.js
@@ -0,0 +1,28 @@
+import { FETCH_STREAMS_REQUEST, FETCH_STREAMS_SUCCESS, FETCH_STREAMS_FAILURE } from '../actionTypes';
+import axios from '../../axiosApi';
+
+const fetchStreamsRequest = () => ({
+  type: FETCH_STREAMS_REQUEST,
+});
+
+const fetchStreamsSuccess = (payload) => ({
+  type: FETCH_STREAMS_SUCCESS,
+  payload,
+});
+
+const fetchStreamsFailure = (payload) => ({
+  type: FETCH_STREAMS_FAILURE,
+  payload,
+});
+
+export const fetchStreams = () => {
+  async (dispatch) => {
+    dispatch(fetchStreamsRequest());
+    try {
+      const response = await axios('/streams');
+      dispatch(fetchStreamsSuccess(response.data));
+    } catch (error) {
+      dispatch(fetchStreamsFailure(error));
+    }
+  };
+};
