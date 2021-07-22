6ba7e5c7c6ed97a01c2d3be4b25e756864f04e7e
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-07-22 17:59:54 +0600

    добавил reducer

---
+++ src/store/reducers/paymentsReducer.js
@@ -0,0 +1,17 @@
+import { FETCH_PAYMENTS_SUCCESS, FETCH_PAYMENTS_FAILURE } from '../actionTypes';
+
+const initialState = {
+  payments: [],
+  error: null,
+};
+
+export default function streamsReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PAYMENTS_SUCCESS:
+      return { ...state, payments: action.payload, error: null };
+    case FETCH_PAYMENTS_FAILURE:
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+}
