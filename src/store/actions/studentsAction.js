4223662a0d93b89b7ce5c4ff675f630f5c17c576
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-24 01:09:26 +0600

    Переименованы параметры поиска студентов

--- src/store/actions/studentsAction.js
+++ src/store/actions/studentsAction.js
@@ -44,7 +44,7 @@ export const fetchStudents = () => async (dispatch) => {
 export const getStudentsByParams = (firstName, lastName) => async (dispatch) => {
   try {
     dispatch(fetchStudentsRequest());
-    const response = await axios.get(`/students/?firstname=${firstName}&lastname=${lastName}`);
+    const response = await axios.get(`/students/?firstName=${firstName}&lastName=${lastName}`);
     dispatch(fetchStudentsSuccess(response.data));
   } catch (error) {
     if (error.response && error.response.data) {
