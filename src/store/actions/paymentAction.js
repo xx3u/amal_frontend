28e88a75187e4c6d917a7720417d54aceb303210
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-23 16:47:35 +0600

    добавлены actions на добавление новой оплаты

--- src/store/actions/paymentAction.js
+++ src/store/actions/paymentAction.js
@@ -1,5 +1,12 @@
 import axios from '../../axiosApi';
-import { FETCH_PAYMENTS_SUCCESS, FETCH_PAYMENTS_REQUEST, FETCH_PAYMENTS_FAILURE } from '../actionTypes';
+import {
+  FETCH_PAYMENTS_SUCCESS,
+  FETCH_PAYMENTS_REQUEST,
+  FETCH_PAYMENTS_FAILURE,
+  ADD_NEW_STUDENT_FAILURE,
+  ADD_NEW_PAYMENT_REQUEST,
+  ADD_NEW_PAYMENT_SUCCESS,
+} from '../actionTypes';
 
 const fetchPaymentsRequest = () => ({ type: FETCH_PAYMENTS_REQUEST });
 
@@ -20,5 +27,19 @@ export const fetchPayments = () => async (dispatch) => {
     dispatch(fetchPaymentsSuccess(response.data));
   } catch (error) {
     dispatch(fetchPaymentsFailure(error));
+  }
+};
+
+const addNewPaymentRequest = () => ({ type: ADD_NEW_PAYMENT_REQUEST });
+const addNewPaymentSuccess = (data) => ({ type: ADD_NEW_PAYMENT_SUCCESS, data });
+const addNewPaymentFailure = (error) => ({ type: ADD_NEW_STUDENT_FAILURE, error });
+
+export const addNewPayment = (newPayment) => async (dispatch) => {
+  dispatch(addNewPaymentRequest());
+  try {
+    const response = await axios.post('/payments', newPayment);
+    dispatch(addNewPaymentSuccess(response.data));
+  } catch (error) {
+    dispatch(addNewPaymentFailure(error));
   }
 };
