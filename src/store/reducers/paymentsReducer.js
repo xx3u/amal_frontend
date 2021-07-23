cf7bad34e297d79afe9af859686e99741971acde
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-23 16:51:05 +0600

    добавлены reducers по новой оплате

--- src/store/reducers/paymentsReducer.js
+++ src/store/reducers/paymentsReducer.js
@@ -1,8 +1,10 @@
-import { FETCH_PAYMENTS_SUCCESS, FETCH_PAYMENTS_FAILURE } from '../actionTypes';
+import { FETCH_PAYMENTS_SUCCESS, FETCH_PAYMENTS_FAILURE, ADD_NEW_PAYMENT_FAILURE } from '../actionTypes';
+import { ADD_NEW_PAYMENT_REQUEST, ADD_NEW_PAYMENT_SUCCESS } from './../actionTypes';
 
 const initialState = {
   payments: [],
   error: null,
+  loading: false,
 };
 
 export default function paymentsReducer(state = initialState, action) {
@@ -11,6 +13,12 @@ export default function paymentsReducer(state = initialState, action) {
       return { ...state, payments: action.payload, error: null };
     case FETCH_PAYMENTS_FAILURE:
       return { ...state, error: action.payload };
+    case ADD_NEW_PAYMENT_REQUEST:
+      return { ...state, loading: true };
+    case ADD_NEW_PAYMENT_SUCCESS:
+      return { ...state, loading: false };
+    case ADD_NEW_PAYMENT_FAILURE:
+      return { ...state, loading: false, error: action.data };
     default:
       return state;
   }
