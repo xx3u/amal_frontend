3b670b692fa66afff72aef775dea5f8fd85c3435
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-23 16:38:42 +0600

    добавлен компонент PaymentForm

--- src/pages/PaymentsPage/PaymentsPage.js
+++ src/pages/PaymentsPage/PaymentsPage.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import PaymentsContainer from '../../containers/PaymentsContainer/PaymentsContainer';
+import PaymentForm from './../../containers/Forms/Payment/PaymentForm';
 
 const useStyles = makeStyles((theme) => ({
   content: {
     marginTop: theme.spacing(4),
   },
   addBtn: {
-    marginBottom: 40
-  }
+    marginBottom: 40,
+  },
 }));
 
 const PaymentsPage = () => {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+  const closePaymentForm = () => {
+    setIsOpen(false);
+  };
 
   return (
     <Grid container className={classes.content}>
-      <Button variant='contained'  color='default' className={classes.addBtn}>Добавить оплату</Button>
+      <PaymentForm isOpen={isOpen} handleClose={closePaymentForm} title='Добавить оплату' />
+      <Button variant='contained' color='default' className={classes.addBtn} onClick={() => setIsOpen(true)}>
+        Добавить оплату
+      </Button>
       <PaymentsContainer />
     </Grid>
   );
