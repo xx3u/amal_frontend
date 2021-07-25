d3afabaf94808be31be3f76f22618f9c10c67859
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-25 23:22:17 +0600

    добавлена страница по Оплатам по студенту

--- src/pages/PaymentsByStudent/PaymentsByStudent.js
+++ src/pages/PaymentsByStudent/PaymentsByStudent.js
@@ -1,11 +1,33 @@
-import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import { Grid, makeStyles, Button } from '@material-ui/core';
+import { Link, useParams } from 'react-router-dom';
+import PaymentsByStudentId from '../../containers/PaymentsByStudentId/PaymentsByStudentId';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginBottom: theme.spacing(4),
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  item: {
+    marginTop: theme.spacing(2),
+  },
+  holder: {
+    marginTop: 20,
+  },
+}));
 
 const PaymentsByStudent = () => {
+  const classes = useStyles();
+  const { id } = useParams();
+
   return (
-    <Grid container>
-      <Grid item>
-        <Typography>Детализация оплат по студенту</Typography>
+    <Grid container className={classes.holder}>
+      <PaymentsByStudentId id={id} />
+      <Grid container className={classes.item} item>
+        <Button variant='contained' component={Link} to='/admin-app/payments' color='default'>
+          Вернуться к платежам
+        </Button>
       </Grid>
     </Grid>
   );
