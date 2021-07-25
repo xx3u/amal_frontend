421d3194e41b0fae7e701a18fcd8060b1f4ce88e
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-25 23:20:18 +0600

    добавлен метод getPaymentsByStudentId

--- src/store/reducers/paymentsReducer.js
+++ src/store/reducers/paymentsReducer.js
@@ -10,6 +10,9 @@ import {
   UPDATE_PAYMENT_REQUEST,
   UPDATE_PAYMENT_SUCCESS,
   UPDATE_PAYMENT_FAILURE,
+  GET_PAYMENTS_BY_STUDENT_ID_REQUEST,
+  GET_PAYMENTS_BY_STUDENT_ID_SUCCESS,
+  GET_PAYMENTS_BY_STUDENT_ID_FAILURE,
 } from './../actionTypes';
 
 const initialState = {
@@ -17,6 +20,7 @@ const initialState = {
   error: null,
   loading: false,
   payment: {},
+  paymentsByStudent: [],
 };
 
 export default function paymentsReducer(state = initialState, action) {
@@ -42,6 +46,12 @@ export default function paymentsReducer(state = initialState, action) {
     case UPDATE_PAYMENT_SUCCESS:
       return { ...state, loading: false };
     case UPDATE_PAYMENT_FAILURE:
+      return { ...state, error: action.error, loading: false };
+    case GET_PAYMENTS_BY_STUDENT_ID_REQUEST:
+      return { ...state, loading: true };
+    case GET_PAYMENTS_BY_STUDENT_ID_SUCCESS:
+      return { ...state, paymentsByStudent: action.data, loading: false };
+    case GET_PAYMENTS_BY_STUDENT_ID_FAILURE:
       return { ...state, error: action.error, loading: false };
     default:
       return state;
