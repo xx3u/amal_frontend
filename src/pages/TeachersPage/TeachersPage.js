e67ab079b98946c7db6bcf5895d25a5084ae55fe
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-25 15:51:37 +0600

    Создана страница TeachersPage

---
+++ src/pages/TeachersPage/TeachersPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeachersContainer from '../../containers/TeachersContainer/TeachersContainer';
+
+const TeachersPage = () => {
+  return (
+    <div className='TeachersPage'>
+      <TeachersContainer />
+    </div>
+  );
+};
+
+export default TeachersContainer;
