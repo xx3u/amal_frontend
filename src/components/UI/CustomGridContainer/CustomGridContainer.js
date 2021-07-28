6c0574c6e4ede55bfdbd39cd9562059252ada7bc
Yuliya Khvan <yuliya.khv@gmail.com>
2021-07-28 20:05:05 +0600

    исправлен CustomGridContainer

--- src/components/UI/CustomGridContainer/CustomGridContainer.js
+++ src/components/UI/CustomGridContainer/CustomGridContainer.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { green } from '@material-ui/core/colors';
 
-const CustomGridContainer = ({ text, icon }) => {
+const useStyles = makeStyles(() => ({
+  iconStatus: {
+    verticalAlign: 'middle',
+    marginLeft: 10,
+  },
+}));
+
+const CustomGridContainer = ({ status }) => {
+  const classes = useStyles();
+
   return (
     <Grid container justifyContent='space-between'>
       <Grid item xs={6}>
-        <Typography>{text}</Typography>
+        <Typography>{status ? 'Оплачено' : 'Не оплачено'}</Typography>
       </Grid>
       <Grid item xs={6}>
-        {icon}
+        {status ? (
+          <CheckCircleOutlineIcon className={classes.iconStatus} style={{ color: green[500] }} />
+        ) : (
+          <ErrorOutlineIcon color='secondary' className={classes.iconStatus} />
+        )}
       </Grid>
     </Grid>
   );
