23733ace2659baec3645c9608af8a0ea32ba9856
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-01 19:38:37 +0600

    добавлен функционал получения и отображения список предметов

--- src/containers/LessonsContainer/LessonsContainer.js
+++ src/containers/LessonsContainer/LessonsContainer.js
@@ -5,13 +5,16 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { fetchGroups } from '../../store/actions/groupsAction';
+import { fetchSubjects } from '../../store/actions/subjectsAction';
 
 const LessonsContainer = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchGroups());
+    dispatch(fetchSubjects());
   }, [dispatch]);
   const { groups } = useSelector((state) => state.groups);
+  const { subjects } = useSelector((state) => state.subjects);
 
   return (
     <Grid item xs={12}>
@@ -21,6 +24,13 @@ const LessonsContainer = () => {
         getOptionLabel={(option) => option.groupName}
         style={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label='Группа' variant='outlined' placeholder='Выберите' />}
+      />
+       <Autocomplete
+        id='subjects-lessons'
+        options={subjects}
+        getOptionLabel={(option) => option.subjectName}
+        style={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label='Предмет' variant='outlined' placeholder='Выберите' />}
       />
     </Grid>
   );
