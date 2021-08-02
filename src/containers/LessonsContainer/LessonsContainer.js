75328b4e1d53dbfa59fc3f1fcd0ae7ee81bccb26
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-02 17:26:49 +0600

    добавлены стили

--- src/containers/LessonsContainer/LessonsContainer.js
+++ src/containers/LessonsContainer/LessonsContainer.js
@@ -1,14 +1,38 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Grid, TextField } from '@material-ui/core';
+import { Box, Grid, TextField, Typography } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import DateFnsUtils from '@date-io/date-fns';
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { makeStyles } from '@material-ui/core/styles';
 import { fetchGroups } from '../../store/actions/groupsAction';
 import { fetchSubjects } from '../../store/actions/subjectsAction';
 import { getTeachersBySubject } from '../../store/actions/teachersActions';
 
+const useStyles = makeStyles(() => ({
+  lessonsData: {
+    margin: 50,
+    paddingLeft: 100,
+    paddingRight: 400,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  autocomplete: {
+    marginBottom: 20,
+  },
+  dates: {
+    padding: 15,
+  },
+  dateBox: {
+    marginBottom: 50,
+  },
+  dateText: {
+    marginBottom: 5,
+  },
+}));
+
 const LessonsContainer = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -38,36 +62,48 @@ const LessonsContainer = () => {
   }, [subjectId]);
 
   return (
-    <Grid item xs={12}>
-      <Autocomplete
-        id='groups-lessons'
-        options={groups}
-        getOptionLabel={(option) => option.groupName}
-        noOptionsText={'список пуст'}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label='Группа' variant='outlined' placeholder='Выберите' />}
-      />
-      <Autocomplete
-        id='subjects-lessons'
-        options={subjects}
-        getOptionLabel={(option) => option.subjectName}
-        onChange={onChangeSubject}
-        noOptionsText={'список пуст'}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label='Предмет' variant='outlined' placeholder='Выберите' />}
-      />
-      <Autocomplete
-        id='teachers-lessons'
-        options={teachersBySubject}
-        getOptionLabel={(option) => option.firstName}
-        noOptionsText={'выберите сначала предмета'}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label='Учитель' variant='outlined' placeholder='Выберите' />}
-      />
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker value={startDate} onChange={(date) => setStartDate(date)} format='yyyy/MM/dd' />
-        <KeyboardDatePicker value={endDate} onChange={(date) => setEndDate(date)} format='yyyy/MM/dd' />
-      </MuiPickersUtilsProvider>
+    <Grid item xs={12} className={classes.lessonsData}>
+      <Grid>
+        <Autocomplete
+          id='groups-lessons'
+          className={classes.autocomplete}
+          options={groups}
+          getOptionLabel={(option) => option.groupName}
+          noOptionsText={'список пуст'}
+          style={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label='Группа' variant='outlined' placeholder='Выберите' />}
+        />
+        <Autocomplete
+          id='subjects-lessons'
+          className={classes.autocomplete}
+          options={subjects}
+          getOptionLabel={(option) => option.subjectName}
+          onChange={onChangeSubject}
+          noOptionsText={'список пуст'}
+          style={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label='Предмет' variant='outlined' placeholder='Выберите' />}
+        />
+        <Autocomplete
+          id='teachers-lessons'
+          options={teachersBySubject}
+          getOptionLabel={(option) => option.firstName}
+          noOptionsText={'выберите сначала предмета'}
+          style={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label='Учитель' variant='outlined' placeholder='Выберите' />}
+        />
+      </Grid>
+      <Grid className={classes.dates}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Box className={classes.dateBox}>
+            <Typography className={classes.dateText}>Дата начала</Typography>
+            <KeyboardDatePicker value={startDate} onChange={(date) => setStartDate(date)} format='yyyy/MM/dd' />
+          </Box>
+          <Box>
+            <Typography className={classes.dateText}>Дата конца</Typography>
+            <KeyboardDatePicker value={endDate} onChange={(date) => setEndDate(date)} format='yyyy/MM/dd' />
+          </Box>
+        </MuiPickersUtilsProvider>
+      </Grid>
     </Grid>
   );
 };
