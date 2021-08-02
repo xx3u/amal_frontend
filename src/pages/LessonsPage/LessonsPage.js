dc0088e4c58a3072f566cd001a939c4469b854ee
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-02 21:35:29 +0600

    добавил стили

--- src/pages/LessonsPage/LessonsPage.js
+++ src/pages/LessonsPage/LessonsPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import LessonsContainer from '../../containers/LessonsContainer/LessonsContainer';
 
+const useStyles = makeStyles(() => ({
+  title: {
+    margin: 20,
+    marginLeft: 150,
+    fontWeight: 'bold',
+  },
+}));
+
 const LessonsPage = () => {
+  const classes = useStyles();
+
   return (
     <Grid>
-      <Typography variant='h5'>Групповые занятия</Typography>
+      <Typography variant='h5' className={classes.title}>
+        Групповые занятия
+      </Typography>
       <LessonsContainer />
     </Grid>
   );
