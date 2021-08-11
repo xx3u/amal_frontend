e1bd4d495be4e580df7dc864a5a881aa250a747e
Yuliya Khvan <yuliya.khv@gmail.com>
2021-08-11 21:41:08 +0600

    убран лишний код, добавлено название, добавлен fetchTeachers

--- src/containers/LessonsByTeacher/LessonsByTeacher.js
+++ src/containers/LessonsByTeacher/LessonsByTeacher.js
@@ -5,11 +5,9 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import DateFnsUtils from '@date-io/date-fns';
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { makeStyles } from '@material-ui/core/styles';
-import { fetchGroups } from '../../store/actions/groupsAction';
-import { fetchSubjects } from '../../store/actions/subjectsAction';
-import { getTeachersBySubject } from '../../store/actions/teachersActions';
+import { fetchTeachers } from '../../store/actions/teachersActions';
 import { getWeekdates } from '../../helpers/helpers';
-import { addNewLesson, fetchLessonsByGroupId } from '../../store/actions/lessonsAction';
+import { fetchLessonsByGroupId } from '../../store/actions/lessonsAction';
 import ScheduleTable from '../../components/ScheduleTable/ScheduleTable';
 
 const useStyles = makeStyles(() => ({
@@ -26,13 +24,10 @@ const LessonsByTeacher = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchGroups());
-    dispatch(fetchSubjects());
+    dispatch(fetchTeachers());
   }, [dispatch]);
 
-  const { groups } = useSelector((state) => state.groups);
-  const { subjects } = useSelector((state) => state.subjects);
-  const { teachersBySubject } = useSelector((state) => state.teachers);
+  const { teachers } = useSelector((state) => state.teachers);
 
   const [selectedDate, setSelectedDate] = useState(new Date());
 
@@ -48,22 +43,6 @@ const LessonsByTeacher = () => {
     lesson.startTime && dispatch(fetchLessonsByGroupId(lesson.groupId, lesson.startTime, lesson.endTime));
   }, [lesson.groupId, dispatch]);
 
-  const onClickHandler = async (startTime, endTime) => {
-    const newLesson = {
-      groupId: lesson.groupId,
-      subjectId: lesson.subjectId,
-      teacherId: lesson.teacherId,
-      startTime: startTime,
-      endTime: endTime,
-    };
-    // await dispatch(addNewLesson(newLesson));
-    dispatch(fetchLessonsByGroupId(lesson.groupId, lesson.startTime, lesson.endTime));
-  };
-
-  useEffect(() => {
-    lesson.subjectId && dispatch(getTeachersBySubject(lesson.subjectId));
-  }, [lesson.subjectId]);
-
   useEffect(() => {
     const copyDate = new Date(selectedDate);
     const newCopyDate = new Date(selectedDate);
@@ -76,13 +55,15 @@ const LessonsByTeacher = () => {
 
   return (
     <>
-      <Typography variant='h4' paragraph={true} className={classes.title}>Расписание по Учителям</Typography>
+      <Typography variant='h5' paragraph={true} className={classes.title}>
+        Расписание по Учителям
+      </Typography>
       <Grid container spacing={3} className={classes.container}>
         <Grid item xs={3}>
           <Autocomplete
             id='teachers-lessons'
             className={classes.autoComplTeacher}
-            options={teachersBySubject}
+            options={teachers}
             getOptionLabel={(option) => `${option.firstName} ${option.lastName}` || ''}
             onChange={(event, value) => setLesson((state) => ({ ...state, teacherId: value?.id }))}
             noOptionsText={'выберите сначала предмета'}
@@ -101,7 +82,7 @@ const LessonsByTeacher = () => {
           </MuiPickersUtilsProvider>
         </Grid>
       </Grid>
-      <ScheduleTable selectedParams={lesson} onClickHandler={onClickHandler} />
+      <ScheduleTable selectedParams={lesson} />
     </>
   );
 };
