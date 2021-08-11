6fb20e3049385b7c474a15d209a4aa8afb8eeb34
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-08-11 15:38:46 +0600

    удален закомментированные, лишные коды

--- src/pages/LessonsPage/LessonsPage.js
+++ src/pages/LessonsPage/LessonsPage.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import LessonsContainer from '../../containers/LessonsContainer/LessonsContainer';
-// import ScheduleTable from '../../components/ScheduleTable/ScheduleTable';
 
 const useStyles = makeStyles(() => ({
-  lessonsPage: {},
   title: {
     margin: 20,
     marginLeft: 150,
@@ -17,7 +15,7 @@ const LessonsPage = () => {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.lessonsPage}>
+    <Grid>
       <Typography variant='h5' className={classes.title}>
         Групповые занятия
       </Typography>
