18950862680de30c9e4a194ece456c84e4d2885e
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-16 14:51:55 +0600

    добавил функции в экшн файле

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,6 +1,6 @@
 import axios from '../../axiosApi';
 import { push } from 'connected-react-router';
-import { CREATE_USER_FAILURE, CREATE_USER_SUCCESS } from '../actionTypes';
+import { CREATE_USER_FAILURE, CREATE_USER_SUCCESS, LOGIN_USER_FAILURE, LOGIN_USER_SUCCESS } from '../actionTypes';
 
 const createUserSuccess = (user) => {
   return { type: CREATE_USER_SUCCESS, user };
@@ -21,6 +21,25 @@ export const createUser = (userData) => {
       } else {
         dispatch(createUserFailure(error));
       }
+    }
+  };
+};
+
+const loginUserSuccess = (user) => {
+  return { type: LOGIN_USER_SUCCESS, user };
+};
+const loginUserFailure = (error) => {
+  return { type: LOGIN_USER_FAILURE, error };
+};
+
+export const loginUser = (userData) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post('/users/login', userData);
+      dispatch(loginUserSuccess(response.data));
+      dispatch(push('/'));
+    } catch (error) {
+      dispatch(loginUserFailure(error));
     }
   };
 };
