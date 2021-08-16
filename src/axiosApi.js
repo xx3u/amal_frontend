38ac0bbb34c289ce4bf50e54f5d2c487ab0299e0
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-16 20:49:31 +0600

    добавил в axios файл функцию перехватичик токена при запросах

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -4,4 +4,10 @@ const instance = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
+instance.interceptors.request.use((req) => {
+  const users = store.getState().users;
+  if (users.user) req.headers['Authorization'] = `Bearer ${users.user.token}`;
+  return req;
+});
+
 export default instance;
