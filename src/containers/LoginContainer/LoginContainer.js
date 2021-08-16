38e16eb7f74e4694962798ee82d70850f1505d22
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-16 14:56:37 +0600

    добавлен контейнер логина

---
+++ src/containers/LoginContainer/LoginContainer.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUser } from '../../store/actions/usersActions';
+import FormElement from '../../components/UI/Form/FormElement/FormElement';
+import UserForm from '../../components/UserForm/UserForm';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  alert: {
+    width: '100%',
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.users.loginError);
+
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const inputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
+    await dispatch(loginUser({ ...state }));
+  };
+  return (
+    <UserForm onSubmit={submitFormHandler} title='Войти' button='Войти'>
+      {error && (
+        <Alert severity='error' className={classes.alert}>
+          <AlertTitle>Error</AlertTitle>
+          {error.response.data.details ? error.response.data.details[0].message : error.response.data.error}
+        </Alert>
+      )}
+      <Grid item xs={12}>
+        <FormElement
+          name='username'
+          value={state.username}
+          onChange={inputChangeHandler}
+          label='Username'
+          type='text'
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <FormElement
+          name='password'
+          value={state.password}
+          onChange={inputChangeHandler}
+          label='Password'
+          type='password'
+        />
+      </Grid>
+    </UserForm>
+  );
+};
+
+export default Login;
