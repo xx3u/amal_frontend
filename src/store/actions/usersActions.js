1f003392da2a95b903047d2d377dfc020f49c60d
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-19 13:35:38 +0600

    убрал user при регистрации

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -9,8 +9,8 @@ import {
   LOGOUT_USER_SUCCESS,
 } from '../actionTypes';
 
-const createUserSuccess = (user) => {
-  return { type: CREATE_USER_SUCCESS, user };
+const createUserSuccess = () => {
+  return { type: CREATE_USER_SUCCESS };
 };
 const createUserFailure = (error) => {
   return { type: CREATE_USER_FAILURE, error };
@@ -19,9 +19,9 @@ const createUserFailure = (error) => {
 export const createUser = (userData) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post('/users/register', userData);
-      dispatch(createUserSuccess(response.data));
-      dispatch(push('/login'));
+      await axios.post('/users/register', userData);
+      dispatch(createUserSuccess());
+      dispatch(push('/'));
     } catch (error) {
       dispatch(createUserFailure(error));
     }
