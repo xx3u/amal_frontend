fb3ca5a8e6f76eebf7f0fdc10cb7a18c3ed15ffa
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-20 23:14:06 +0600

    Добавил компонент AddCard

---
+++ src/components/ScheduleTable/AddCard/AddCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { CardActions, IconButton } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    minWidth: 200,
+    minHeight: 100,
+    height: '100%',
+  },
+  cardActions: {
+    display: 'flex',
+    'flex-direction': 'column',
+
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+const EmptyCard = ({ allowed, onClickHandler }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant='outlined'>
+      {allowed && (
+        <CardActions className={classes.cardActions}>
+          <>
+            <IconButton onClick={onClickHandler}>
+              <AddCircleIcon fontSize='large' color='disabled' />
+            </IconButton>
+          </>
+        </CardActions>
+      )}
+    </Card>
+  );
+};
+
+export default EmptyCard;
