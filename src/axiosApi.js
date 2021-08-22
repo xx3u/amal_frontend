4d2346915eae494004a1e7daaf8d1c0b20eb393f
Yuliya <yuliyahvan@yahoo.com>
2021-08-22 19:25:50 +0600

    resolve merge conflicts

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
-<<<<<<< HEAD
 import config from './config';
-=======
 import store from './store/configureStore';
->>>>>>> 96f33b3da53f305ba92f3c0ac539c6153329759f
 
 const instance = axios.create({
   baseURL: config.BACKEND_BASE_URL || 'http://localhost:8080',
