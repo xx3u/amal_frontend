cd8f887a64ae5e38095e25b42bf2b4552080b3a6
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-25 20:11:32 +0600

    Добавил таблицу

--- src/containers/StudentAttendanceContainer/StudentAttendanceContainer.js
+++ src/containers/StudentAttendanceContainer/StudentAttendanceContainer.js
@@ -1,77 +1,65 @@
-import DateFnsUtils from '@date-io/date-fns';
-import { Grid, TextField } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { endOfMonth, startOfMonth } from 'date-fns';
+import { Grid } from '@material-ui/core';
+import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import SimpleTable from '../../components/UI/SimpleTable/SimpleTable';
 import { transformToUTC } from '../../helpers/helpers';
-import { fetchGroups } from '../../store/actions/groupsAction';
-import { fetchLessonsByGroupId, setInitLessons } from '../../store/actions/lessonsAction';
+import StudentAttendanceSelects from './StudentAttendanceSelects';
 
 const StudentAttendanceContainer = () => {
   const dispatch = useDispatch();
-  const [selectedGroup, setSelectedGroup] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(transformToUTC(new Date()));
-  const [monthInterval, setMonthInterval] = useState({
-    start: null,
-    end: null,
+  const [students, setStudents] = useState([]);
+  const lessons = useSelector((state) =>
+    state.lessons.lessons.sort((a, b) => {
+      return new Date(a.startTime) - new Date(b.startTime);
+    })
+  );
+
+  const objLessons = lessons.reduce((lessons, lesson) => {
+    return { ...lessons, [lesson.startTime]: lesson.id };
+  }, {});
+
+  const onSelectedGroupHandler = (selectedGroup) => {
+    console.log(selectedGroup);
+    selectedGroup?.Students && setStudents(selectedGroup.Students);
+  };
+
+  const rows = students.map((student) => {
+    return { studentName: `${student.lastName} ${student.firstName} `, ...objLessons };
   });
 
-  const groups = useSelector((state) => state.groups.groups);
-  const students = useSelector((state) => state.groups.groups.students);
-  const lessons = useSelector((state) => state.lessons.lessons);
+  console.log('rows:', rows);
+  console.log('students:', students);
 
-  useEffect(() => {
-    dispatch(fetchGroups());
-  }, [dispatch]);
+  console.log(lessons);
 
-  useEffect(() => {
-    setMonthInterval({
-      start: transformToUTC(startOfMonth(selectedDate)),
-      end: transformToUTC(endOfMonth(selectedDate)),
-    });
-  }, [selectedDate]);
+  const LessonColumns = lessons.map((lesson) => {
+    console.log(lesson.startTime);
+    return {
+      field: lesson.startTime,
+      headerName: format(transformToUTC(new Date(lesson.startTime)), 'dd MMM hh:mm', { locale: ru }),
+      width: 100,
+    };
+  });
 
-  useEffect(() => {
-    dispatch(setInitLessons());
-    selectedDate &&
-      selectedGroup?.id &&
-      dispatch(
-        fetchLessonsByGroupId(selectedGroup.id, monthInterval.start.toISOString(), monthInterval.end.toISOString())
-      );
-  }, [selectedGroup, monthInterval, dispatch]);
+  const columns = [
+    {
+      field: 'studentName',
+      headerName: 'ФИО',
+      width: 100,
+    },
+    ...LessonColumns,
+  ];
 
   return (
-    <Grid container item spacing={3}>
-      <Grid item xs={3}>
-        <Autocomplete
-          id='groups-lessons'
-          value={selectedGroup}
-          onChange={(event, value) => {
-            setSelectedGroup(value);
-          }}
-          options={groups}
-          getOptionLabel={(option) => option.groupName}
-          noOptionsText={'список пуст'}
-          renderInput={(params) => <TextField {...params} label='Группа' variant='outlined' placeholder='Выберите' />}
-        />
+    <>
+      <Grid container item spacing={3}>
+        <StudentAttendanceSelects onSelectedGroupHandler={onSelectedGroupHandler} />
       </Grid>
-      <MuiPickersUtilsProvider locale={ru} utils={DateFnsUtils}>
-        <Grid item xs={3}>
-          <KeyboardDatePicker
-            label='Дата'
-            variant='inline'
-            inputVariant='outlined'
-            fullWidth
-            value={selectedDate}
-            onChange={(date) => setSelectedDate(transformToUTC(date))}
-            format='dd MMM yyyy'
-          />
-        </Grid>
-      </MuiPickersUtilsProvider>
-    </Grid>
+
+      <SimpleTable rows={rows} columns={columns} />
+    </>
   );
 };
 
