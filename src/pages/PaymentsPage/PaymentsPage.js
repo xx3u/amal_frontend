425c5751b9cbb41cbfe70bcd6dd534a5b1f7e1a6
Zaur <ravencount@gmail.com>
2021-08-29 11:16:31 +0600

    useSelector deleted

--- src/pages/PaymentsPage/PaymentsPage.js
+++ src/pages/PaymentsPage/PaymentsPage.js
@@ -3,7 +3,6 @@ import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import PaymentsContainer from '../../containers/PaymentsContainer/PaymentsContainer';
 import CreatePaymentForm from '../../containers/Forms/Payment/CreatePaymentForm';
-import { useSelector } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   content: {
